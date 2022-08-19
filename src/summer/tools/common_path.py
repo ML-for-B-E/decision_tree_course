171bd39a6e0732c3d4b4d91efc6fc4687877d011
kevin.kpakpo <kevin.kpakpo@descartesunderwriting.com>
2022-08-19 21:37:48 +0000

    chore: rename notebook folder

--- src/summer/tools/common_path.py
+++ src/summer/tools/common_path.py
@@ -3,5 +3,5 @@ from pathlib import Path
 LIB_PATH = Path(__file__).parents[1]
 SRC_PATH = LIB_PATH.parent
 ROOT_PATH = SRC_PATH.parent
-NOTEBOOK_PATH = Path(ROOT_PATH, "cours_de_stats")
+NOTEBOOK_PATH = Path(ROOT_PATH, "notebook")
 DATA_PATH = Path(ROOT_PATH, "data")
