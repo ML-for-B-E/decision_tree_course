95585d0e955ffb3dee990d8763acede7eabbe5b3
Kpakpo Akouete <adkevinkpakpo@gmail.com>
2022-08-20 02:39:09 +0200

    ci: authentication for the CI
    CI accepts only raw json

--- src/cloud_io/gcp/base.py
+++ src/cloud_io/gcp/base.py
@@ -1,9 +1,11 @@
+import json
 import os
 from pathlib import Path
 
 import google.cloud.storage as gcs
-from cloud_io.tools.error import CloudIOError
 from cloud_io.gcp.filesystem import prepare_filesystem
+from cloud_io.tools.error import CloudIOError
+from google.oauth2.service_account import Credentials
 
 
 def download_blob(
@@ -36,6 +38,16 @@ def get_bucket(bucket_name: str) -> gcs.Bucket:
 def get_client() -> gcs.Client:
     if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ.keys():
         client = gcs.Client()
+    elif "GOOGLE_APPLICATION_CREDENTIALS_JSON" in os.environ.keys():
+        client = _access_with_raw_json()
     else:
         raise CloudIOError("Please setup your credentials.")
     return client
+
+
+def _access_with_raw_json() -> gcs.Client:
+    raw_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    parsed_json = raw_json.replace("\n", "\\n")
+    service_account_info = json.loads(parsed_json)
+    credentials = Credentials.from_service_account_info(service_account_info)
+    return gcs.Client(credentials=credentials, project=credentials.project_id)
